a83ac4ddc20883217b385d25e4eff436dce17362
Uraniumus <irma121@ya.ru>
2021-10-17 23:55:48 +0300

    init

---
+++ solution/index.js
@@ -0,0 +1,11 @@
+// solution/index.js
+module.exports = function (Homework) {
+
+    // вспомогательные фукнции и т.д.
+
+    return (array, fn, initialValue, cb) => {
+
+        // асинхронный reduce
+
+    }
+}
